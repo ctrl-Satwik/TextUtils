6054234b4c69b6a69f2d3e820fa84441e1e53f0b
ctrl-Satwik <satwik.saurav.91@gmail.com>
2024-07-02 19:28:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+// import logo from './logo.svg';
+import { useState } from 'react';
+import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+// import {
+//    BrowserRouter, 
+//    Route, 
+//    Routes 
+//   } from "react-router-dom";
+
+
+function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#004e53';
+      // document.title = 'TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      // document.title = 'TextUtils - Light Mode';
+    }
+  }
+  return (
+    <>
+    {/* <BrowserRouter> */}
+  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+  <div className="container my-3">
+  {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}>
+            </Route> */}
+            {/* <Route exact path="/" element={}>
+            </Route>
+          </Routes> */}
+          <TextForm heading= "Enter the text to analyze" mode={mode}/>
+          
+  </div>
+  {/* </BrowserRouter> */}
+  
+    </>
+  );
+}
+
+export default App;
